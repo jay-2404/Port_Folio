487a641bed44065c133335e6cc0e8ff4f139e4ce
jayendraVroomster <jayendra@vroomster.com>
2025-02-07 12:15:35 +0530

    Components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
+const Body = styled.div`
+  background-color: ${(props) => props.theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Body>
+        <Hero />
+        <Wrapper>
+          <Skills />
+        </Wrapper>
+      </Body>
+    </ThemeProvider>
   );
 }
 
